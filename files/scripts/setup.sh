8f534ff7bbbca4704a47b8450ae7f2f9e08ebcdd
Justin Lloyd <justin@imf.la>
2016-09-30 11:55:44 -0700

    Quick hack to add install some Ruby gems, upgrade the version of Ruby installed and install a zip (compression) utility

--- files/scripts/setup.sh
+++ files/scripts/setup.sh
@@ -4,4 +4,8 @@
 # vagrant@vagrantpress:~$ phpunit --version
 # PHPUnit 4.4.4 by Sebastian Bergmann.
 echo "PATH=$PATH:/usr/local/bin/vendor/bin/" >> /home/vagrant/.bashrc
-sudo apt-get install zip
+sudo apt-add-repository -y ppa:brightbox/ruby-ng
+sudo apt-get update
+sudo apt-get -y install zip libmysqlclient-dev ruby2.3 ruby2.3-dev
+sudo gem install httparty mysql2 json activesupport
+
